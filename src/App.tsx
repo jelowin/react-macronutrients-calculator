7e0313881b699e709d4f67305d19718f91e91e9d
Jenifer López <jenifer.lopez@adevinta.com>
2022-04-23 19:28:35 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Container, Heading } from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+  return (
+    <Container maxW="1280px" centerContent>
+      <Heading as="h1" size="3xl">
+        Hello World!
+      </Heading>
+    </Container>
+  );
+}
+
+export default App;
